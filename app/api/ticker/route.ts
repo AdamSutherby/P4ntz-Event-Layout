3248e14dcb8a9ddcb7cedd2986b117cd2682d391
AdamSutherby <AdamSutherby@gmail.com>
2025-01-23 16:26:11 -0700

    Update route.ts

--- app/api/ticker/route.ts
+++ app/api/ticker/route.ts
@@ -1,14 +1,14 @@
-import { NextResponse } from 'next/server'
+// import { NextResponse } from 'next/server'
 
-export async function GET() {
-  return new NextResponse(null, { status: 204 })
-}
+// export async function GET() {
+//   return new NextResponse(null, { status: 204 })
+// }
 
-export async function POST(request: Request) {
+// export async function POST(request: Request) {
 
-  // Option 2: If you might use the data later
-  // const data = await request.json()
-  // Perform operations with data here...
+//   // Option 2: If you might use the data later
+//   // const data = await request.json()
+//   // Perform operations with data here...
 
-  return NextResponse.json({ success: true })
-}
+//   return NextResponse.json({ success: true })
+// }
